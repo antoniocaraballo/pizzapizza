79af5600481367273292cd17db8c546953ce4b3c
antoniocaraballo <antonio@MacBook-Pro.attlocal.net>
2021-11-02 13:12:04 -0400

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var element = document.getElementById("top");
+    element.classList.toggle("top");
+    
+    var element = document.getElementById("mid");
+    element.classList.toggle("mid");
+  
+    var element = document.getElementById("bottom");
+    element.classList.toggle("bottom");
+
+    var element = document.getElementById("navbar");
+    element.classList.toggle("nav-active");
+
+    
+  }
